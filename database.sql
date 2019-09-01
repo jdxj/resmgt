7182ebced8e6c4a020c777d278554cf89d80f615
jdxj <985759262@qq.com>
2019-09-01 16:48:05 +0800

    refactor: 使用 gorm 惯例

--- database.sql
+++ database.sql
@@ -23,10 +23,10 @@ CREATE TABLE `files` (
   `category` int(11) DEFAULT NULL,
   `content` mediumtext NOT NULL,
   `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `datetime` datetime NOT NULL,
+  `date_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `files_FK` (`owner`),
   KEY `files_FK_1` (`category`),
   CONSTRAINT `files_FK` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `files_FK_1` FOREIGN KEY (`category`) REFERENCES `categorys` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
