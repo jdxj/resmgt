ff61ab42a972673e6e96375b40d642f52c70e159
jdxj <985759262@qq.com>
2019-09-01 15:25:37 +0800

    fix: 解决 categorys 表无法插入第一个数据的问题

--- database.sql
+++ database.sql
@@ -3,24 +3,24 @@ CREATE TABLE `users` (
   `name` varchar(100) NOT NULL,
   `password` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `categorys` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `parent` int(11) NOT NULL,
+  `pid` int(11) DEFAULT NULL,
   `owner` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `categorys_FK_1` (`owner`),
-  KEY `categorys_FK` (`parent`),
-  CONSTRAINT `categorys_FK` FOREIGN KEY (`parent`) REFERENCES `categorys` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `categorys_FK_1` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  KEY `categorys_FK` (`owner`),
+  KEY `categorys_FK_1` (`pid`),
+  CONSTRAINT `categorys_FK` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `categorys_FK_1` FOREIGN KEY (`pid`) REFERENCES `categorys` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner` int(11) NOT NULL,
-  `category` int(11) DEFAULT '0',
+  `category` int(11) DEFAULT NULL,
   `content` mediumblob NOT NULL,
   `name` varchar(100) NOT NULL,
   `date` datetime NOT NULL,
@@ -29,4 +29,4 @@ CREATE TABLE `files` (
   KEY `files_FK_1` (`category`),
   CONSTRAINT `files_FK` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `files_FK_1` FOREIGN KEY (`category`) REFERENCES `categorys` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
